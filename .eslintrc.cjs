f982827107af190a6ad97b7357ebcb09b62f8bb3
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-10-23 15:35:20 +0900

    chore(lint): enable import/no-default-export = error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,12 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: [
+    '@typescript-eslint',
+    'import'
+  ],
+  rules: {
+    'import/no-default-export': 'error',
+  },
   root: true,
 };
