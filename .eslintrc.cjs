3aaf7510e3b567845edfabb315162028520ab848
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-10-23 16:08:53 +0900

    chore(lint): add react-hooks lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
   plugins: [
